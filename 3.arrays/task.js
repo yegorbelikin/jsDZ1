45171a51e52a6f8e79431a91d66a3c7a4ab9fdba
Yegor <yegor.belikin@gmail.com>
2025-07-28 20:21:09 +0500

    Corrected 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
   
-}
+  const result = arr1.every((element, i) => 
+         (arr1.length === arr2.length && element === arr2[i]));
+  return result;
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
   
-}
+function getUsersNamesInAgeRange(users, gender) {
+    let result = users
+    .filter(element => element.gender === gender)
+    .map(element => element = element.age)
+    .reduce((acc, element, i, ages) => {
+      if (i != ages.length - 1) {
+        return acc + element;
+      } else {
+        return (acc + element) / ages.length;
+      }
+    }, 0);
+      return result;
+  }
