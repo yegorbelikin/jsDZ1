977a346e8ee904573c0bc0883ce28c0db564f1a4
Yegor <yegor.belikin@gmail.com>
2025-08-01 08:24:01 +0500

    Corrected 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+        return;
+    } else {
+        this.marks.push(...marksToAdd)
+    }
   
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+
+    const sum = this.marks.reduce((acc, element) => acc + element, 0);
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
   
 }
