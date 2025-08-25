578682459b3bd6bb720534a91003bfa8aa8e7849
Yegor <yegor.belikin@gmail.com>
2025-08-25 17:51:27 +0500

    Corrected 8.deco

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,51 @@
 //Задача № 1
+const md5 = require("js-md5");
+
 function cachingDecoratorNew(func) {
+    let cache = [];
   
-}
+    function wrapper(...args) {
+        const hash = md5(JSON.stringify(args)); // получаем правильный хеш c помощью функции md5
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+        if (objectInCache) { // если элемент найден
+            console.log("Из кеша: " + objectInCache.value, cache); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кеша: " + objectInCache.value;
+        }
+  
+        let result = func(...args); // в кеше результата нет — придётся считать
+        cache.push({hash: hash, value: result}); // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+          cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
+  }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  function wrapper(...args){
+    wrapper.allCount++;
+    if (timeoutId) {
+      console.log("Уже есть таймаут", args);
+      clearTimeout(timeoutId);
+    }
+      if(!timeoutId){
+        console.log("Первый сигнал", args);
+        func.call(this, ...args);
+        wrapper.count++;
+      }
+
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+        wrapper.count++
+      }, delay);
+    }
+    return wrapper;
+  }
+
+
